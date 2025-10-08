d6c318b927170ce19f793a59f4ed247a64cd737b
Amber-222 <ST10287325@vcconnect.edu.za>
2025-10-08 19:17:40 +0200

    xss protection started
    * installed xss package and adjusted some cors and helmet configs

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,8 +1,14 @@
+const xssProtect = require('xss') //npm i xss for protection
 const Payment = require('../models/paymentModel.js') //connect model to controller
 
 //CREATE PAYMENT (post)
 const createPayment = async(req, res) => {
-    const {customerName, amount, currency, provider} = req.body //collect input from frontend form
+    let {customerName, amount, currency, provider} = req.body //collect input from frontend form
+
+    //INPUT SANITISING FOR XSS ATTACKS
+    customerName = xss.customerName
+    currency = xss.currency
+    provider = xss.provider
 
     if (!customerName || !amount || !currency || !provider) { //check for nulls in payment fields and double check a user is logged in
         return res.status(400).json({message: "Please ensure all required fields are filled out"})
@@ -10,7 +16,7 @@ const createPayment = async(req, res) => {
 
     try {
         const payment = await Payment.create({customerName, amount, currency, provider, verified: false}) //create the payment object 
-        return res.status(201).json(payment) //returnt that the object was created and logged in db
+        return res.status(201).json({message: "Payment created successfully.", payment: payment}) //returnt that the object was created and logged in db
     }
     catch (error) {
         return res.status(500).json({error: error.message}) //if soemthing goes wrong, return eror details
@@ -20,7 +26,12 @@ const createPayment = async(req, res) => {
 //VERIFY PAYMENT (put)
 const verifyPayment = async(req, res) => {
     const id = req.params.id
-    const {customerName, amount, currency, provider, verified} = req.body //pull payment object from frontend
+    let {customerName, amount, currency, provider, verified} = req.body //pull payment object from frontend
+
+    //INPUT SANITISING FOR XSS ATTACKS
+    customerName = xss.customerName
+    currency = xss.currency
+    provider = xss.provider
 
     try {
         const payment = await Payment.findByIdAndUpdate(id, {verified}, {new: true}) //find the payment from the db and update where verified field updates
--- backend/middleware/securityMiddleware.js
+++ backend/middleware/securityMiddleware.js
@@ -2,7 +2,7 @@ const cors = require('cors') //imports cors to allow access into the frontend fr
 const helmet = require('helmet') //import helmet to protect the frontend
 
 const corsOptions = {
-    origin: '*',
+    origin: 'https://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }
@@ -13,10 +13,20 @@ app.use(helmet({
             useDefaults: true,
             directives: {
                 'default-src': ["'self'"], //allows scripts from the website itself but nowhere else
+                'script-src': ["'self'"],
+                'style-src': ["'self'", "'unsafe-inline'"], //allows inline styling where needed
+                'img-src': ["'self'", "data:"], //allows images and base64 date
+                'object-src': ["'none'"],
                 'frame-ancestors': ["'none'"] //prevents this webiste from being embedded in another website to prevent clickjacking
             }
         },
-        featurePolicy: {
+        crossOriginOpenerPolicy: { 
+            policy: "same-origin" 
+        },
+        crossOriginResourcePolicy: { 
+            policy: "same-origin"
+        },
+        permissionsPolicy: {
             features: {
                 geolocation: ["'none'"], //block access to location apis in website, prevents access to physical devices
                 microphone: ["'none'"]
@@ -26,7 +36,9 @@ app.use(helmet({
         frameguard: { //prevent website from being put in an iframe 
             action: 'deny'
         },
-        ieNoOpen: true
+        ieNoOpen: true,
+        noSniff: true,
+        xssFilter: true //basic xss protection enabled
     }))
 
     app.use(cors(corsOptions))
