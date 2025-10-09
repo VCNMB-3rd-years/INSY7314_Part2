6f1bf73937c82f40eb2f5ab32d836b898d55a7eb
jcvheerden <ST10316473@vcconnect.edu.za>
2025-10-09 16:28:12 +0200

    Testing

--- backend/controllers/customerController.js
+++ backend/controllers/customerController.js
@@ -71,8 +71,8 @@ const login = async (req, res) => {
         //Reset brute-force counter on successful login
         req.brute.reset(()=> {
             const safeCustomer = customerData.toObject ? customerData.toObject() : customerData;
-            delete safeCustomer.userPassword;
-            return res.status(200).json({ message: "Login successful", customer: safeCustomer });
+            delete safeCustomer.userPassword;
+            return res.status(200).json({ message: "Login successful", customer: safeCustomer });
         })
 
     } catch (error) {
