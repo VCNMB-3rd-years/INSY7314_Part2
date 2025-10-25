0f2f757f4f762429340dbded06a8e5c91f982380
jcvheerden <ST10316473@vcconnect.edu.za>
2025-10-25 16:24:35 +0200

    Fixed the SSL certifcates
    HTTPS and the certificates are present in employee_frontend.
    can successfully add employees

--- employee_frontend/src/services/apiService.js
+++ employee_frontend/src/services/apiService.js
@@ -1,6 +1,6 @@
 import api from '../interfaces/axiosInstance.js' //call in axios interface
 
 export const getPendingPayments = () => api.get('/payments')
-export const registerEmployee = (employee) => api.post('/employee/register', employee)
+export const registerEmployee = (employee) => api.post('/employee/registerEmployee', employee)
 export const loginCustomer = (customer) => api.post('/customer/login', customer)
 export const loginEmployee = (employee) => api.post('/employee/loginEmployee', employee)
