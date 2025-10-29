9bccfa106675e3fe80425835908863ece9eeef93
Amber-222 <ST10287325@vcconnect.edu.za>
2025-10-29 07:56:01 +0200

    i had certs issues ..

--- employee_frontend/src/App.jsx
+++ employee_frontend/src/App.jsx
@@ -6,7 +6,7 @@ import WelcomePage from './pages/welcomePage.jsx'
 import Login from './pages/login.jsx'
 import Navbar from './components/Navbar.jsx'
 import PermissionDenied from './pages/permissionDenied.jsx';
-
+import ProtectedRoute from './components/protectedRoute.jsx'
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
         <div className="page-container">
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/paymentPortal" element={<PaymentPortal />} />
-        <Route path="/register" element={<RegisterEmployee />} />
+        <Route path="/paymentPortal" element={<ProtectedRoute> <PaymentPortal /> </ProtectedRoute>} />
+        <Route path="/register" element={ <ProtectedRoute> <RegisterEmployee /> </ProtectedRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/permissionDenied" element={<PermissionDenied/>} />
       </Routes> 
